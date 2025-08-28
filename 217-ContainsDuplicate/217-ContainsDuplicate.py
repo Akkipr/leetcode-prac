c79193c2e4b453a41400cf9bd8b2498c2126fe29
Akkipr <akshayanpraba6@gmail.com>
2025-08-28 02:00:31 -0400

    Update 217-ContainsDuplicate.py

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,4 +1,4 @@
-# Last updated: 8/28/2025, 1:59:45 AM
+# Last updated: 8/28/2025, 2:00:30 AM
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         s = set()
@@ -13,6 +13,11 @@ class Solution:
 
 
         '''
+        Old Solution (More runttime memory)
+
+        What I learned:
+        HashSets are a LOT faster than arrays! 
+        
         nums.sort()
         for i in range (len(nums)-1):
             if (nums[i]==nums[i+1]):
