84b4ab6809e81870aa30c9bc7bcbe5110f3296d4
Akkipr <akshayanpraba6@gmail.com>
2025-08-28 21:20:20 -0400

    Update 242-ValidAnagram.py

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,11 +1,49 @@
-# Last updated: 8/28/2025, 8:46:39 PM
+# Last updated: 8/28/2025, 9:20:19 PM
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Solution 2
+        dict_t = {}
+        dict_s = {}
+
+        if len(s) == len(t):
+            for i in range(len(s)):
+                if (s[i] in dict_s):
+                    dict_s[s[i]] = dict_s[s[i]] + 1
+                if (t[i] in dict_t):
+                    dict_t[t[i]] = dict_t[t[i]] + 1
+                if (t[i] not in dict_t):
+                    dict_t[t[i]] = 1
+                if (s[i] not in dict_s):
+                    dict_s[s[i]] = 1
+            return dict_s == dict_t
+        else:
+            return False
+
+
+
+        '''
+        # Solution 1
         s = sorted(s)
         s = "".join(s)
         t = sorted(t)
         t = "".join(t)
 
-        return True if s==t else False
+        return s==t
+
+
+        if (s[i] not in dict_s or t[i] not in dict_t):
+                    if (t[i] not in dict_t):
+                        dict_t[t[i]] = 1
+                    if (s[i] not in dict_s):
+                        dict_s[s[i]] = 1
+                    continue
+                else:
+                    if (s[i] in dict_s):
+                        dict_s[s[i]] = dict_s[s[i]] + 1
+                    if (t[i] in dict_t):
+                        dict_t[t[i]] = dict_t[t[i]] + 1
+        '''
+
+        
 
         
