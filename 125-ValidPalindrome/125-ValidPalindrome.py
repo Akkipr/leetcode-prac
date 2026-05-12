5493eb6128954bd63651e9f71568f908b8396688
Akkipr <akshayanpraba6@gmail.com>
2026-05-12 00:16:00 -0400

    Update 125-ValidPalindrome

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,6 +1,18 @@
-# Last updated: 5/12/2026, 12:13:05 AM
+# Last updated: 5/12/2026, 12:16:00 AM
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum(): l += 1
+            while l < r and not s[r].isalnum(): r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+
+        '''
+        My attempt - works but runtime is bad
         s=s.lower()
         for x in s:
             if (x.isalnum()):
@@ -14,5 +26,6 @@ class Solution:
                 return False
             j-=1
         return True
+        '''
             
             
