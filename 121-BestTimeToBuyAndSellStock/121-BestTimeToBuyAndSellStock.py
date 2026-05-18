8e946535575521ff3b02ea943740af73a5e456c9
Akkipr <akshayanpraba6@gmail.com>
2026-05-18 04:33:14 -0400

    Update 121-BestTimeToBuyAndSellStock

--- 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
@@ -1,7 +1,22 @@
-# Last updated: 5/18/2026, 4:28:59 AM
+# Last updated: 5/18/2026, 4:33:15 AM
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+                
+        return max_profit
+
+
+
+        '''
+        My attempt - same thing but using max()
         maxSum = 0
         minimum = prices[0]
         
@@ -14,6 +29,9 @@ class Solution:
             maxSum = max(summ,maxSum)
         
         return maxSum
+                
+        '''
+        
                 
 
         
